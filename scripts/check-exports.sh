ae5ae88e8e9d80dadcf2323e72003cf6b9602a50
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2018-08-29 18:25:32 +0200

    ci: check exported symbols
    This runs a check during the ci run that checks if the functions marked
    for export, functions in the public headers and the symbols in the SOs
    symbol table are the same.

---
+++ scripts/check-exports.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# ---HELP---
+# check if exported symbols match up with header content and the shared object symbol table
+# Must be run from rtrlibs root dir.
+# ---HELP---
+
+ERROR=0
+
+# Functions annoted with RTRLIB_EXPORT
+EXPORTS=$(
+    grep -r '^RTRLIB_EXPORT' rtrlib |
+    grep -Po '(?<= ).*?(?=\(.*?)' | grep -o  '\w*$' | # extract function name
+    sort)
+
+# Functions found in public headers
+HEADER_SYMBOLS=$(
+    cat $(find rtrlib -iname '*.h' -type f ! -name '*_private.h' ! -name '*tommy*') | # cat all public headers
+    gcc -o - -xc -fpreprocessed  -dD -E -P  - | # strip comments via gcc
+    grep -Po '(?<= ).*?(?=\(.*?)' | grep -o '\w*$' | # extract function name
+    sort)
+
+# Symbols found in librtrs dynamic export table
+SO_SYMBOLS=$(nm -g --defined-only librtr.so | awk '{ print $3 }' | sort)
+
+# check for equality of $EXPORTS AND $HEADER_SYMBOLS
+diff -q <(echo "$EXPORTS") <(echo "$HEADER_SYMBOLS") > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "Functions annotated for export are not equal to functions found in public header!"
+    comm <(echo "$EXPORTS") <(echo "$HEADER_SYMBOLS")
+    ERROR=1
+fi
+
+echo
+
+# check for equality of $HEADER_SYMBOLS and $SO_SYMBOLS
+diff -q <(echo "$HEADER_SYMBOLS") <(echo "$SO_SYMBOLS") > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "Functions found in public header are not equal to exported functions!"
+    comm <(echo "$HEADER_SYMBOLS") <(echo "$SO_SYMBOLS")
+    ERROR=1
+fi
+
+
+exit $ERROR
